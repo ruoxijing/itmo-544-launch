59ae6162ca99d75a7244fd877421e192caeee11b
ruoxijing <rjing@hawk.iit.edu>
2015-10-04 13:03:16 -0500

    This is launch.sh

---
+++ launch.sh
@@ -0,0 +1,1 @@
+echo aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --user-data file://install-env.sh --debug
