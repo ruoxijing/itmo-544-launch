09332323fdd9856466cd4b9fa3534b848898e22d
ruoxijing <rjing@hawk.iit.edu>
2015-10-28 22:35:28 -0500

    Modified launch.sh

--- launch.sh
+++ launch.sh
@@ -33,18 +33,20 @@ aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-ext
 #create rds-instances
 mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
 
-if [ ${#dbInstanceARR[@]} -gt 0 ]
-   then 
-   LENGTH=${#dbInstanceARR[@]}
-   
+#if [ ${#dbInstanceARR[@]} -gt 0 ]
+#   then 
+#echo ${#dbInstanceARR[@]}
 
-       for (( i=0; i<${LENGTH}; i++));
+   LENGTH=${#dbInstanceARR[@]}
+#echo $LENGTH	
+
+       for (( i=0; i<=${LENGTH}; i++));
       do
-      if [ ${dbInstanceARR[i]} == "jrx-db" ]
+      if [[ ${dbInstanceARR[i]} == "jrx-db" ]]
      then 
       echo "db exists"
      else
      aws rds create-db-instance --db-instance-identifier jrx-db --db-instance-class db.t1.micro --engine MySQL --master-username rjing --master-user-password mypoorphp --allocated-storage 5
       fi  
      done
-fi
+#fi
