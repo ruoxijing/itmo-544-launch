4e4a41c19dea841e862364f5dfa076b05e6dacb4
ruoxijing <rjing@hawk.iit.edu>
2015-11-04 16:12:33 -0600

    Added setup.php

--- launch.sh
+++ launch.sh
@@ -54,11 +54,13 @@ mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep 
 
        for (( i=0; i<=${LENGTH}; i++));
       do
-      if [[ ${dbInstanceARR[i]} == "jrx-db" ]]
+      if [[ ${dbInstanceARR[i]} == "jrxdb" ]]
      then 
       echo "db exists"
      else
-     aws rds create-db-instance --db-instance-identifier jrx-db --db-instance-class db.t1.micro --engine MySQL --master-username rjing --master-user-password mypoorphp --allocated-storage 5
+     aws rds create-db-instance --db-instance-identifier jrxdb --db-instance-class db.t1.micro --engine MySQL --master-username rjing --master-user-password mypoorphp --allocated-storage 5
       fi  
      done
 #fi
+
+php ./setup.php
