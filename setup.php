51f514353d5067d4200d30d4f30ba0b9a3020ae2
ruoxijing <rjing@hawk.iit.edu>
2015-11-08 20:55:04 -0600

    Modified without endpoint

--- setup.php
+++ setup.php
@@ -1,6 +1,7 @@
 <?php
 
 require '../itmo-544-env/vendor/autoload.php';
+#require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
@@ -9,12 +10,11 @@ $client = RdsClient::factory(array(
 ));
 
 
-#$result = $client->describeDBInstances(array(
-#    'DBInstanceIdentifier' => 'itmo544jrxdb',
+#$rds = RdsClient::factory(array(
+#'version'=>'latest',
+#'region'  => 'us-west-2'
 #));
 
-
-#$endpoint = ""; 
 
 
 #foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
@@ -22,14 +22,28 @@ $client = RdsClient::factory(array(
 #    echo "============". $ep . "================";
 #    $endpoint = $ep;
 #}
+#$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'jrx-db',
+#)];
 
+#$result = $client->describeDBInstances(array(
+#    'DBInstanceIdentifier' => 'jrx-db',
+#));
 
+#$result = $rds->describeDBInstances([
+#    'DBInstanceIdentifier' => 'jrx-db',
+#]);
 
+#$endpoint = ""; 
+#print_r($result);
+#$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+#    echo "============\n". $endpoint . "================";
+
+#print_r($endpoint);  
 echo "begin database";
-#$link = mysqli_connect($endpoint,"controller","ilovebunnies","itmo544db") or die("Error " . mysqli_error($link));
-$link = mysqli_connect("jrxdb.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","jrxdb") or die("Error " . mysqli_error($link));
+#$link = mysqli_connect($endpoint,"controller","ilovebunnies","itmo544db",3306) or die("Error " . mysqli_error($link));
+$link = mysqli_connect("jrx-db.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
+#$link = mysqli_connect($endpoint,"rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
 /* check connection */
-sleep(1);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
@@ -45,7 +59,7 @@ else {
 
 }
 */
-$create_table = 'CREATE TABLE IF NOT EXISTS items  
+$create_table = 'CREATE TABLE IF NOT EXISTS mp1tb  
 (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(200) NOT NULL,
@@ -54,13 +68,9 @@ $create_table = 'CREATE TABLE IF NOT EXISTS items
     s3rawurl VARCHAR(255) NOT NULL,
     s3finishedurl VARCHAR(255) NOT NULL,
     status INT NOT NULL,
-    issubscribed INT NOT NULL
+    issubscribed INT NOT NULL,
     PRIMARY KEY(id)
 )';
-
-
-
-
 $create_tbl = $link->query($create_table);
 if ($create_table) {
 	echo "Table is created or No error returned.";
