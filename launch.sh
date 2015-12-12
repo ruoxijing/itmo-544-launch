5d411ffe791baeb87d25d7020de1bf8d56174fbc
ruoxijing <rjing@hawk.iit.edu>
2015-12-12 12:43:17 -0600

    Modified

--- launch.sh
+++ launch.sh
@@ -65,13 +65,10 @@ mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep 
 
 #sns service
 ARN=(`aws sns create-topic --name mp2`)
-
 echo "This is the ARN: $ARN"
 
 aws sns set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --attribute-value mp2
-
 aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 13127215036
-
 aws sns publish --topic-arn $ARN --message "best code"
 
 #cloudwatch AddCapacity
